ab4903166acf79011632eb0c6877215bfda87e90
Felipe Ribeiro <felipedejesusifgoiano@gmail.com>
2022-09-16 21:20:13 -0300

    modified:   src/App.js 	new file:   src/components/layout/Footer.js 	new file:   src/components/layout/Footer.module.css 	new file:   src/components/layout/Navbar.js 	new file:   src/components/layout/Navbar.module.css 	new file:   src/components/pages/Projects.js

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,31 @@
+import {Link} from 'react-router-dom'
+import Company from '../pages/Company'
+import Contact from '../pages/Contact'
+import Home from '../pages/Home'
+import NewProject from '../pages/NewProject'
+
+import Container from './Container'
+
+import styles from './Navbar.module.css'
+import logo from '../../img/costs_logo.png'
+import Projects from '../pages/Projects'
+
+function Navbar(){
+
+    return(
+        <nav className={styles.navbar}>
+            <Container>
+                <Link to='/' element={<Home/>} >
+                    <img src={logo} alt="Costs" />
+                </Link>
+                <ul className={styles.list}>
+                    <li className={styles.item}><Link to='/' element={<Home/>} >Home</Link></li>
+                    <li className={styles.item}><Link to='/projects' element={<Projects/>} >Meus Projetos</Link></li>
+                    <li className={styles.item}><Link to='/contact' element={<Contact/>} >Empresa</Link></li>
+                    <li className={styles.item}><Link to='/company' element={<Company/>} >Contato</Link></li>
+                </ul>
+            </Container>
+        </nav>
+    )
+}
+export default Navbar
