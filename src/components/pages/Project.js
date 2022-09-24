6f158ff58f03968a8817857c0f4a7fe394b53d45
Felipe Ribeiro <felipedejesusifgoiano@gmail.com>
2022-09-24 15:11:40 -0300

    EXIBINDO SERVIÇOS
    	modified:   db.json
    	modified:   src/components/layout/Container.module.css
    	modified:   src/components/pages/Project.js
    	modified:   src/components/project/ProjectCard.module.css
    	new file:   src/components/service/ServiceCard.js

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -2,7 +2,7 @@ import {parse, v4 as uuidv4} from 'uuid'
 
 import styles from './Project.module.css'
 
-import {json, useParams} from 'react-router-dom'
+import {useParams} from 'react-router-dom'
 import {useEffect, useState} from 'react'
 
 import Loading from '../layout/Loading'
@@ -10,12 +10,14 @@ import Container from '../layout/Container'
 import Message from '../layout/Message'
 import ProjectForm from '../project/ProjectForm'
 import ServiceForm from '../service/ServiceForm'
+import ServiceCard from '../service/ServiceCard'
 
 function Project() {
 
     const { id } = useParams()
     
     const [project, setProject] = useState([])
+    const [services, setServices] = useState([])
     const [showProjectForm, setShowProjectForm] = useState(false)
     const [showServiceForm, setShowServiceForm] = useState(false)
     const [message, setMessage] = useState()
@@ -28,9 +30,10 @@ function Project() {
                 headers: {
                     'Content-Type': 'application/json'
                 },
-            }).then((resp) => resp.json()).
-            then((data) => {
+            }).then((resp) => resp.json())
+            .then((data) => {
                 setProject(data)
+                setServices(data.services)
             })
             .catch(err => console.log(err))
         }, 500);
@@ -96,10 +99,13 @@ function Project() {
             body: JSON.stringify(project)
         }).then((resp) => resp.json())
         .then((data) => {
-            //exibir os serviços
-            console.log(data)
+            setShowServiceForm(false)
         })
         .catch(err => console.log(err))
+    }
+    
+    function removeService() {
+
     }
 
     function toogleProjectForm(){
@@ -162,7 +168,20 @@ function Project() {
                     </div>
                     <h2>Serviços</h2>
                     <Container customClass="start">
-                        <p>Itens de serviços</p>
+                        {services.length > 0 &&
+                        services.map((service) => (
+                            <ServiceCard 
+                            id={service.id}
+                            name={service.name}
+                            cost={service.cost}
+                            description={service.description}
+                            key={service.id}
+                            handleRemove={removeService}
+                            />
+                        ))
+
+                        }
+                        {services.length === 0 && <p>Não há serviços cadastrados.</p> }
                     </Container>
                 </Container>
             </div>
